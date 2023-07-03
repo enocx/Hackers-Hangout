4c21c9aa78d150eed5883065929111c954b105c0
Enoch <enoch@gmail.com>
2023-07-03 20:35:56 +0530

    Add and rename few custom colors in the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,12 @@ module.exports = {
         custom:{
           nblack: '#191919',
           nred: '#DF5452',
+          nredbac: '#362422',
           nwhite: '#D4D4D4',
-          ncallout: '#362422',
           nblue: '#5585A7',
+          ngreenbac: '#243D30',
+          nyellow: '#CA9849',
+          norange: '#C77D48',
         }
       }
     },
